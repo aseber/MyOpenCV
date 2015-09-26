3b4ad3ec6d3cea1abbbf9bf178d8ff50211f3847
aseber <aseber@techsouth.cc>
2015-09-25 21:33:10 -0400

    More work on orb matching

--- orb_matcher.py
+++ orb_matcher.py
@@ -7,11 +7,36 @@ print __version__
 # This python script requires OpenCV 3.0.0 to run!
 # Make sure you install the optional git repo for OpenCV so you can use SIFT ect.
 
+class sliderGui():
+    def __init__(self):
+        cv2.namedWindow("Sliders")
+
+        cv2.createTrackbar('Table values', 'Sliders', 1, 25, self.nothing)
+        cv2.setTrackbarPos('Table values', 'Sliders', 6)
+        cv2.createTrackbar('Key Size', 'Sliders', 1, 100, self.nothing)
+        cv2.setTrackbarPos('Key Size', 'Sliders', 100)
+        cv2.createTrackbar('Probe Level' , 'Sliders', 1, 5, self.nothing)
+        cv2.setTrackbarPos('Probe Level', 'Sliders', 1)
+
+        cv2.createTrackbar('K num', 'Sliders', 1, 50, self.nothing)
+        cv2.setTrackbarPos('K num', 'Sliders', 2)
+
+        cv2.createTrackbar('Match count', 'Sliders', 1, 1000, self.nothing)
+        cv2.setTrackbarPos('Match count', 'Sliders', 10)
+    def returnVals(self):
+        return {'Table values':cv2.getTrackbarPos('Table values', 'Sliders',), 'Key Size':cv2.getTrackbarPos('Key Size', 'Sliders'), 'Probe Level':cv2.getTrackbarPos('Probe Level', 'Sliders'), 'K num':cv2.getTrackbarPos('K num', 'Sliders'), 'Match count':cv2.getTrackbarPos('Match count', 'Sliders')}
+
+    def nothing(self, x):
+        pass
+
 def main():
     camera = cv2.VideoCapture(-1)   # Tells OpenCV to retrieve the first camera it finds, values like 0, 1, 2... ect. let you specify a camera if multiple are present
     detector = cv2.ORB_create()                 # Initializes ORB in OpenCV, you can use SIFT, SURF ect. but you *should* pay for them
     # detector = cv2.xfeatures2d.SIFT()
 
+    sliders = sliderGui()
+
+    # Calling crossCheck = true breaks the program. Why?
     bfMatcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = False)    # Brute Force matcher, not very fast
 
     # # FLANN_INDEX_KDTREE = 0  # Useful when we are working with SIFT or SURF
@@ -21,8 +46,8 @@ def main():
     FLAN_INDEX_LSH = 6      # Useful when working with ORB
     index_params = dict(algorithm = FLAN_INDEX_LSH,
                         table_number = 12,  # All of these parameters are default
-                        key_size = 20,
-                        multi_probe_level = 2)
+                        key_size = 100,
+                        multi_probe_level = 1)
 
     search_params = dict(checks = 50)
 
@@ -32,7 +57,8 @@ def main():
     kp1, des1 = detector.detectAndCompute(image1, None)  # Uses ORB to detect interesting points on the image
 
     while camera.isOpened():
-        _, image2 = camera.read()   # Read an image from the camera
+        # _, image2 = camera.read()   # Read an image from the camera
+        image2 = cv2.imread('card.png')
         image2Gray = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)   # Convert image to gray scale
 
         kp2, des2 = detector.detectAndCompute(image2Gray, None)      # Use ORB to detect interesting points on the image
@@ -41,10 +67,13 @@ def main():
         # matches = sorted(matches, key = lambda x:x.distance)  # Sort the matches in order of least distance
         # matches = matches[:10]
 
-        matches = bfMatcher.knnMatch(des1, des2, k = 5)
-        matches = matches[:10]
+        vals = sliders.returnVals()
 
-        # matches = flannMatcher.knnMatch(des1, des2, k = 2)    # FLANN matching instead of brute force
+        matches = bfMatcher.knnMatch(des1, des2, k = vals['K num'])
+        matches = matches[:vals['Match count']]
+
+        # Would love to use FLANN, but it breaks the program
+        # matches = flannMatcher.knnMatch(des1, des2, k = vals['K num'])    # FLANN matching instead of brute force
 
         # inliersMatches = [[0,0] for i in xrange(len(matches))]  # Let's create an empty array so we can filter the matches
 
