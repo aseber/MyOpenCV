dd830af28a169d82f85907786df24deb256a191b
aseber <aseber@techsouth.cc>
2015-10-06 23:17:05 -0400

    Working on tesseract, ported over zbar and cascade files

--- tesseract_test.py
+++ tesseract_test.py
@@ -3,9 +3,18 @@ api = tesseract.TessBaseAPI()
 api.SetOutputName("outputName");
 api.Init(".","eng",tesseract.OEM_DEFAULT)
 api.SetPageSegMode(tesseract.PSM_AUTO)
-mImgFile = "eurotext.jpg"
+mImgFile = "card.png"
 pixImage=tesseract.pixRead(mImgFile)
 api.SetImage(pixImage)
+
+# lst = dir(tesseract)
+
+# f = open("output.txt", 'w')
+
+# for line in lst:
+    # f.write(line)
+    # f.write('\n')
+
 outText=api.GetUTF8Text()
 print("OCR output:\n%s"%outText);
 api.End()
